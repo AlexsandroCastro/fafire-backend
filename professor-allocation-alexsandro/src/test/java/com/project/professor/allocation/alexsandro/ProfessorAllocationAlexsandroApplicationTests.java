908b756899e1b823172ea6efa760dc35e3d1a745
Alexsandro Castro <alexsandrocasttro@gmail.com>
2022-05-30 15:30:52 -0300

    Camada de repositório
    Criação da camada de repositório com teste da camada de repositório

---
+++ professor-allocation-alexsandro/src/test/java/com/project/professor/allocation/alexsandro/ProfessorAllocationAlexsandroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.alexsandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationAlexsandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
