416c457acff19e54118478d2fd71f99cbb54d575
Alexsandro Castro <alexsandrocasttro@gmail.com>
2022-06-01 10:33:20 -0300

    camada de controller
    criação da camada de controller e teste via insomnia e criação swagger

--- professor-allocation-alexsandro/src/main/java/com/project/professor/allocation/alexsandro/repository/AllocationRepository.java
+++ professor-allocation-alexsandro/src/main/java/com/project/professor/allocation/alexsandro/repository/AllocationRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import com.project.professor.allocation.alexsandro.entity.Allocation;
 
 @Repository
